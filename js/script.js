1f40ede8e340380f169f5f868cd8d3d189e203b8
Igor <narghar@gmail.com>
2019-11-15 22:50:51 +0100

    Fix AI Game start screen

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@ const gameBoard2 = document.getElementById("board2");
 const onePlayer = document.getElementById("onePlayer");
 const twoPlayers = document.getElementById("twoPlayers");
 const aiPlayer = document.getElementById("aiPlayer");
+const shipsContainer = document.querySelector('.ships');
 
 
 window.addEventListener('load', (event) => {
     var gameBoard = document.getElementById("board");
     gameBoard.style.display = "none";
     gameBoard2.style.display = "none";
+    shipsContainer.style.display = "none";
 });
 
 onePlayer.addEventListener('click', () => {
@@ -35,7 +37,10 @@ twoPlayers.addEventListener('click', () => {
 aiPlayer.addEventListener('click', () => {
     start.style.display = "none";
     gameBoard.style.display = "grid";
-    gameBoard2.style.display = "grid";
-    const game = new AIPlayer();
-    game.aiPlayerGame();
+    gameBoard.style.margin = "unset";
+    gameBoard2.style.display = "none";
+    shipsContainer.style.display = "flex";
+
+    // const game = new AIPlayer();
+    // game.aiPlayerGame();
 });
