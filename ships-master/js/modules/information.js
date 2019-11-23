ea8a4a7d30ebf7d2178681b14ec199b80033325f
MagdaSzade <37961517+MagdaSzade@users.noreply.github.com>
2019-11-23 21:19:48 +0100

    Delete information.js

--- ships-master/js/modules/information.js
+++
@@ -1,6 +0,0 @@
-function information(txtInfo) {
-    const informationField = document.getElementById("info");
-    informationField.innerHTML = `<h2>${txtInfo}</h2>`;
-}
-
-module.exports = information;
