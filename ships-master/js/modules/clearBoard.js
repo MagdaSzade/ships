56ca171c68e693233ab9289b6baf3b451c2dcaad
MagdaSzade <37961517+MagdaSzade@users.noreply.github.com>
2019-11-23 21:19:10 +0100

    Delete clearBoard.js

--- ships-master/js/modules/clearBoard.js
+++
@@ -1,8 +0,0 @@
-function clearBoard(parentId){
-    let elements = document.getElementById(`${parentId}`);
-    while(elements.children.length > 1){
-        elements.removeChild(elements.children[1]);
-    }
-}
-
-module.exports = clearBoard;
